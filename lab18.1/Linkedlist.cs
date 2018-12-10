48a6eabd88ced9a40b4e01ee2866bdd0c02980d9
unknown <ba7399@gmail.com>
2018-12-10 15:50:46 -0500

    first commit

---
+++ lab18.1/Linkedlist.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab18._1
+{
+
+    public static class Linkedlist
+    {
+
+       
+        
+
+        public static void PrintReverse(this LinkedList<int> list)
+        {
+            Console.WriteLine("original array: \n 1 \n 2 \n 3 \n 2\n 2\n 4\n 5\n 5\n 7\n 8\n 4\n 4\n 1\n 0\n 10");
+            Console.WriteLine("reversed");
+            for (int i = list.Count -1; i >= 0; i--)
+            {
+                
+                    Console.WriteLine(list.ElementAt(i));
+              
+            }
+        }
+
+        public static bool InsertAt(this LinkedList<int> x, int index)
+        {
+
+            LinkedListNode<int> thisnode = x.First;
+            if (x.Count >= index)
+            {
+                for (int i = 0; i < x.Count; i++)
+                {
+                    if (i==index -1)
+                    {
+                        x.AddAfter(thisnode, 1);
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public static class LinkedListExtensions
+    {
+
+        public static bool RemoveAt<T>(this LinkedList<T> list, int index)
+
+        {
+            if (index >= list.Count)
+            {
+                return false;
+            }
+            else
+            {
+                LinkedListNode<T> current = list.First;
+                for (int i = 0; i <= index && current != null; i++)
+                {
+                    if (i != index)
+                    {
+                        current = current.Next;
+                        continue;
+                    }
+                    else
+                    {
+                        list.Remove(current);
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+
+
+        }
+    }
+}
---
+++ lab18.1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab18._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10 };
+            int x = array.Max();
+            int[] y = new int[x + 1];
+            for (int i = 0; i < y.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++) 
+                {
+                    if (array[j] == i)
+                    {
+                        y[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < y.Length; i++)
+            {
+                if (i>0)
+                {
+                    Console.WriteLine($"[{i}]:{y[i]}");
+                }
+            }
+
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.AddFirst(1);
+            numbers.AddLast(2);
+            numbers.AddLast(3);
+            numbers.AddLast(2);
+            numbers.AddLast(2);
+            numbers.AddLast(4);
+            numbers.AddLast(5);
+            numbers.AddLast(5);
+            numbers.AddLast(7);
+            numbers.AddLast(8);
+            numbers.AddLast(4);
+            numbers.AddLast(4);
+            numbers.AddLast(1);
+            numbers.AddLast(0);
+            numbers.AddLast(10);
+
+
+            Dictionary<int, int> numlist = new Dictionary<int, int>();
+            LinkedList<int> findnum = new LinkedList<int>();
+
+            Console.WriteLine("using dictionary:");
+            foreach (var thing in numbers)
+            {
+                if (numlist.ContainsKey(thing))
+                {
+
+                    numlist[thing]++;
+                }
+                else
+                {
+                    
+                    numlist.Add(thing, 1);
+                }
+            }
+                foreach (int thing in numlist.Keys)
+                {
+                Console.WriteLine($"{thing}:{numlist[thing]}");
+                }
+
+            //remove method, but I'm a little at a loss at what to do with a return bool value, if I'm even doing the method correctly.
+            Console.WriteLine("Enter to remove a number");
+            
+            string index = Console.ReadLine();
+            int numindex;
+            if (int.TryParse(index, out numindex))
+            {
+               bool result = numbers.RemoveAt(numindex);
+               Console.WriteLine(result);
+               
+            }
+            else
+            {
+                Console.WriteLine("not a number!");
+            }
+           
+
+            numbers.PrintReverse();
+            numbers.InsertAt(4);
+            // ran out of time for more sophiscated implementation, i am bad at this! 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        } 
+    }
+}
